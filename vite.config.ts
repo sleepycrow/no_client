8a98daf185b18916ae2c7655e7667135572c0829
sleepycrow <sleepycrow@posteo.net>
2025-07-29 22:11:33 +0200

    Set app repo URL

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
 	},
 	define: {
 		__APP_NAME__: JSON.stringify(process.env.npm_package_name),
-		__APP_REPO_URL__: JSON.stringify('https://example.com'),
+		__APP_REPO_URL__: JSON.stringify('https://git.sr.ht/~sleepycrow/no_client'),
 		__APP_VERSION__: JSON.stringify(process.env.npm_package_version),
 		__APP_COMMIT_HASH__: JSON.stringify(getCommitHash()),
 	},
